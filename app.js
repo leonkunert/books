0cba10bd3b8d9db813a5eaf90e8e594fb55443c1
Leon Kunert <info@leonkunert.de>
2014-04-26 23:32:11 +0200

    Logo vergrößert, Index geändert, Styles angepasst.

--- app.js
+++ app.js
@@ -25,43 +25,33 @@ app.get('/partials/:name', function (req, res) {
  */
 
 app.get('/v0/book', function (req, res) {
-    console.log(req.query);
     book.find(req.query, function (err, result) {
-        console.log(err);
-        console.log(result);
         res.json(result);
     });
 });
 
 app.get('/v0/book/:bookId', function (req, res) {
-    console.log(req.params);
     book.findOne({_id:req.params.bookId}, function (err, result) {
-        console.log(err);
-        console.log(result);
         res.json(result);
     });
 });
 
 app.post('/v0/book/create', function (req, res) {
-    console.log(req.body)
     var _book = new book(req.body);
     _book.save(function (err, result) {
-        console.log(err);
-        console.log(result);
         res.json(result);
     });
 });
 
 app.put('/v0/book/update', function (req, res) {
-    console.log(req.body);
     book.findOneAndUpdate({_id:req.body._id}, req.body, function (err, result) {
-        console.log(err);
-        console.log(result);
         res.json(result)
     });
 });
 
-
+/*
+ * Catch everything Angular is doing Routing on the Frontend
+ */
 app.get('*', function (req, res) {
     res.render('index.jade');
 });
